01aee283516f6bbfdc145224c21d1703ec1309b3
mehulgajera77 <mehul.gajera77@gmail.com>
2020-08-20 17:37:38 +0530

    Add One Show() method in Demo1 Class

--- src/com/techforce/demo/Demo1.java
+++ src/com/techforce/demo/Demo1.java
@@ -2,9 +2,20 @@ package com.techforce.demo;
 
 public class Demo1 {
 	
+	/*
+	 * this Method is for the Show the output to the console
+	 * it will not take any argument
+	 */
+	void show()
+	{
+		System.out.println("Show Method");
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println("This is the Demo1");
+		Demo1 demo1=new Demo1();
+		demo1.show();
 	}
 
 }
